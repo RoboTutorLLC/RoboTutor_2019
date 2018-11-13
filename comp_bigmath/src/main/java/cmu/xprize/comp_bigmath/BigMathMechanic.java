4b36f70c6a7c919d3ef39aab5d1fbc4d8eda25eb
Kevin DeLand <kevin.deland@gmail.com>
2018-11-13 16:48:10 -0500

    MATH BORROW: hide temporary helper button

--- comp_bigmath/src/main/java/cmu/xprize/comp_bigmath/BigMathMechanic.java
+++ comp_bigmath/src/main/java/cmu/xprize/comp_bigmath/BigMathMechanic.java
@@ -158,7 +158,7 @@ public class BigMathMechanic {
 
         initializeOnClickListeners();
 
-        initializeBorrowClicker();
+        //initializeBorrowClicker();
 
         initializeWriteInputs();
 
